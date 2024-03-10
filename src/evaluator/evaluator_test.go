06ead36095daa32a794f15a919313bb0901bcafe
Christopher Seven Phiri <chris7phiri@gmail.com>
2024-03-10 20:16:44 +0200

    add tests for closures

--- src/evaluator/evaluator_test.go
+++ src/evaluator/evaluator_test.go
@@ -276,3 +276,13 @@ func TestFunctionApplication(t *testing.T) {
 		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
+
+func TestClosures(t *testing.T) {
+	input := `
+ 	ndondomeko newAdder(x) {
+ 		ndondomeko temp(y) { x + y };
+ 	};
+ 	nambala addTwo = newAdder(2);
+ 	addTwo(2);`
+	testIntegerObject(t, testEval(input), 4)
+}
